eaf8aeb71df8942c413029b5ed9424ab8297b01d
daedal-dev-ru <gerkon.cool@gmail.com>
2024-02-22 00:51:29 +0300

    Add boilerplate

---
+++ src/main/java/ru/daedal/BalanceMicroService/BalanceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.daedal.BalanceMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/daedal/BalanceMicroService/BalanceMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daedal.BalanceMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
